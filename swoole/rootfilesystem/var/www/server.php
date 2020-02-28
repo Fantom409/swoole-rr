d8664e549abed48f6d501d0ca027a50cec4590e8
Anton Samoylenko <a.samoylenko95@gmail.com>
2020-02-28 12:19:19 +0200

    change ports for swoole

--- swoole/rootfilesystem/var/www/server.php
+++ swoole/rootfilesystem/var/www/server.php
@@ -7,10 +7,8 @@ $http = new Swoole\Http\Server("0.0.0.0", 9501);
 $http->on(
     "request",
     function (Swoole\Http\Request $request, Swoole\Http\Response $response) {
-        $memoryPeakUsage = memory_get_peak_usage();
         $response->end(
            (microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'])
-           .'memoryPeakUsage '.$memoryPeakUsage
         );
     }
 );
